b3067a12de53ee6b479c4a93635efb2e467df304
dmitriymafaka <d.zagurskii@gmail.com>
2019-04-26 17:47:55 +0300

    Error handling improved.

--- src/main/java/com/hellokoding/auth/web/IndexController.java
+++ src/main/java/com/hellokoding/auth/web/IndexController.java
@@ -1,18 +1,16 @@
 package com.hellokoding.auth.web;
 
-import com.hellokoding.auth.model.PostFile;
-import com.hellokoding.auth.repository.PostFileRepository;
 import com.hellokoding.auth.repository.PostRepository;
 import com.hellokoding.auth.service.PostFileService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class MainController {
+public class IndexController implements ErrorController {
+
+    private static final String ERROR = "/error";
 
     @Autowired
     private PostRepository postRepository;
@@ -28,5 +26,15 @@ public class MainController {
     @GetMapping("/unsupported")
     public String unsupported() {
         return "unsupported";
+    }
+
+    @GetMapping(ERROR)
+    public String error() {
+        return "no-page-err";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return ERROR;
     }
 }
